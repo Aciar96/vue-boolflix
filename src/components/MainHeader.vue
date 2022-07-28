3c06c2a3942ac3eb7488bb91a3b8a999815acf36
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-07-28 14:49:48 +0200

    first commit

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <input type="text"/> <button>cerca</button>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'MainHeader',
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
